3ea4b0653d0c9920ceff0a824461e53dd76d3046
lyman <m18396531029@163.com>
2019-07-22 23:59:39 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -158,6 +158,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 onPressed: () {
                   Navigator.pushNamed(context, "TextStyleTest");
                 }),
+
+            Image(
+              image: AssetImage("images/avatar.png"),
+              width: 100,
+            ),
+            
+            Image.network("https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
+        width: 100.0,)
           ],
         ),
       ),
